83e086a5582051c672434d7bd0d7a191a0995aa7
Aila204 <ailasuhailanurul0410@gmail.com>
2025-05-23 13:22:27 +0700

    nambah

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     public function login(Request $request): RedirectResponse
     {
         if (Auth::attempt([
-            'email' => $request->email,
+            'email' => $request->username,
             'password' => $request
          ])) {
             $user = User::where('email', $request->email)->first();
